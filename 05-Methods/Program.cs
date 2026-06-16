e2f4837ef90ea68cc2c6661183868b7e3416b18d
Isuru Shehara <isurushehara@gmail.com>
2026-06-16 18:56:00 +0530

    feat: add method and array with examples

---
+++ 05-Methods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+PrintWelcome();
+
+int answer = Add(5, 10);
+
+Console.WriteLine(answer);
+
+double area = CalculateArea(5);
+
+Console.WriteLine(area);
+
+PrintUser("John");
+
+PrintUser();
+
+void PrintWelcome()
+{
+    Console.WriteLine("Welcome");
+}
+
+int Add(int a, int b)
+{
+    return a + b;
+}
+
+double CalculateArea(double radius)
+{
+    return 3.14 * radius * radius;
+}
+
+void PrintUser(string name = "Guest")
+{
+    Console.WriteLine(name);
+}
---
+++ 06-Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+int[] marks =
+{
+    80,
+    90,
+    70,
+    60,
+    100
+};
+
+Console.WriteLine(marks[0]);
+
+Console.WriteLine();
+
+foreach (int mark in marks)
+{
+    Console.WriteLine(mark);
+}
+
+Console.WriteLine();
+
+Array.Sort(marks);
+
+foreach (int mark in marks)
+{
+    Console.WriteLine(mark);
+}
+
+Console.WriteLine();
+
+Console.WriteLine($"Length = {marks.Length}");
