562e40622754f585401c4479b81eb2c6adccf02b
Isuru Shehara <isurushehara@gmail.com>
2026-06-16 18:54:10 +0530

    feat: add conditions and loops with examples

---
+++ 03-Conditions/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+Console.Write("Enter your mark: ");
+
+int mark = int.Parse(Console.ReadLine()!);
+
+if (mark >= 75)
+{
+    Console.WriteLine("Grade A");
+}
+else if (mark >= 65)
+{
+    Console.WriteLine("Grade B");
+}
+else if (mark >= 50)
+{
+    Console.WriteLine("Grade C");
+}
+else
+{
+    Console.WriteLine("Fail");
+}
+
+Console.WriteLine();
+
+Console.Write("Enter day number: ");
+
+int day = int.Parse(Console.ReadLine()!);
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+
+    default:
+        Console.WriteLine("Unknown");
+        break;
+}
---
+++ 04-Loops/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+Console.WriteLine("For Loop");
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+
+Console.WriteLine("While Loop");
+
+int x = 1;
+
+while (x <= 5)
+{
+    Console.WriteLine(x);
+    x++;
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Do While");
+
+int y = 1;
+
+do
+{
+    Console.WriteLine(y);
+    y++;
+
+} while (y <= 5);
+
+Console.WriteLine();
+
+string[] names =
+{
+    "John",
+    "Mike",
+    "Sara"
+};
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
