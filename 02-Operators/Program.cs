3685a61068cb5ca18075aac9da500f3a4f913ab4
Isuru Shehara <isurushehara@gmail.com>
2026-06-16 18:47:20 +0530

    fear(operations): add operation with examples

---
+++ 02-Operators/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+int a = 10;
+int b = 3;
+
+Console.WriteLine("Arithmetic");
+
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+Console.WriteLine(a / b);
+Console.WriteLine(a % b);
+
+Console.WriteLine();
+
+Console.WriteLine("Comparison");
+
+Console.WriteLine(a == b);
+Console.WriteLine(a != b);
+Console.WriteLine(a > b);
+Console.WriteLine(a < b);
+
+Console.WriteLine();
+
+Console.WriteLine("Logical");
+
+bool x = true;
+bool y = false;
+
+Console.WriteLine(x && y);
+Console.WriteLine(x || y);
+Console.WriteLine(!x);
+
+Console.WriteLine();
+
+Console.WriteLine("Assignment");
+
+a += 5;
+
+Console.WriteLine(a);
+
+a--;
+
+Console.WriteLine(a);
+
+a++;
+
+Console.WriteLine(a);
